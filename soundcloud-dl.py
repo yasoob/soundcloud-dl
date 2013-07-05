82ebe456d8553b392734dbe7bcaf85991f13bb5c
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2013-07-06 00:13:26 +0500

    Added an option to download a specific song
    Added an option to download a specific song by inputing its url in this
    script :) enjoy

--- soundcloud-dl.py
+++ soundcloud-dl.py
@@ -18,6 +18,68 @@ entrystring = \
 _useragent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Safari/536.5"
 htmlclient = {"User-Agent":_useragent, "Content-Type":"application/json", "Accept-Encoding":"gzip"}
 
+def single_url(url):
+    """ This method is same as extract_data() (located further down the page) method but in this method we are opening two urls to obtain
+    all the required information about a soundcloud url. It takes only a soundcloud song url as an input and then opens
+    various urls in order to find out the actual url of the soundcloud music file."""
+    global final_result
+    _VALID_URL = r'^(?:https?://)?(?:www\.)?soundcloud\.com/([\w\d-]+)/([\w\d-]+)'
+
+    # checking weather the given url is a valid soundcloud url or not
+    mobj = re.match(_VALID_URL, url)
+    if mobj is None:
+        print u'[Soundcloud-dl]  Invalid URL: %s' %(url)
+        # if the url is invalid then close the program
+        sys.exit()
+
+    # extract uploader (which is in the url)
+    uploader = mobj.group(1)
+
+    # extract simple title (uploader + slug of song title)
+    slug_title =  mobj.group(2)
+    simple_title = uploader + u'-' + slug_title
+    full_title = '%s/%s' % (uploader, slug_title)
+
+    # here we are just simply recreating the url
+    url = 'http://soundcloud.com/%s/%s' % (uploader, slug_title)
+
+    # here we are making the "resolve" url. 
+    # We will open this url and soundcloud will give 
+    # us all the information about the song except its main download url.
+    resolv_url = 'http://api.soundcloud.com/resolve.json?url=' + url + '&client_id=b45b1aa10f1ac2941910a7f0d10f8e28'
+        
+    print u'[Soundcloud-dl]  Resolving the given url...'
+    info_json = requests.get(resolv_url, headers=htmlclient).text
+    
+    info = json.loads(info_json)
+    video_id = info['id']
+
+    # we are opening another url and hopefully this will be the last url
+    # this url will give us the actual link to the mp3 file
+    streams_url = 'https://api.sndcdn.com/i1/tracks/' + str(video_id) + '/streams?client_id=b45b1aa10f1ac2941910a7f0d10f8e28'
+    try:
+        u'[Soundcloud-dl]  Downloading stream definitions',
+        stream_json = requests.get(streams_url, headers=htmlclient).text
+    except:
+        u'[Soundcloud-dl]  unable to download stream definitions'
+        sys.exit()
+
+    streams = json.loads(stream_json)
+    mediaURL = streams['http_mp3_128_url']
+    upload_date = info['created_at']
+
+    # and finally here are the final results in json format
+    final_result = {
+        'id':       info['id'],
+        'url':      mediaURL,
+        'uploader': info['user']['username'],
+        'upload_date': upload_date,
+        'title':    info['title'],
+        'ext':      u'mp3',
+        'description': info['description'],
+    }
+
+
 # Do a search for the songs of a specific user and return the results
 def user_song_search(user_name):
     """This method uses the soundcloud api to search for songs uploaded by a specific
@@ -96,7 +158,7 @@ def downloader(url,filename):
     f = open(file_name, 'wb')
     meta = u.info()
     file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading %s (%s bytes)" %(file_name, file_size)
+    print "[Soundcloud-dl]  Downloading %s (%s bytes)" %(file_name, file_size)
      
     #Calculate downloaded filesize
     file_size_dl = 0
@@ -142,7 +204,7 @@ def convertTime(ms):
 
 # Retrieve all the information from a soundcloud url
 def extract_data(url,video_id,title):
-    """ This method takes a soundcloud song url as an input and then opens
+    """ This method takes a soundcloud song url and some other info about the song as an input and then opens
     various urls in order to find out the actual url of the soundcloud music file."""
     global final_result
     _VALID_URL = r'^(?:https?://)?(?:www\.)?soundcloud\.com/([\w\d-]+)/([\w\d-]+)'
@@ -309,13 +371,27 @@ def for_user():
                 print "\n[Soundcloud-dl]  Download cancelled by the user. "
         sys.exit()
 
+def for_url():
+    # we are asking the user which song does he want to download.
+    song = raw_input("[Soundcloud-dl]  Which url do you want to download?\t")
+    print "[Soundcloud-dl]  Extracting info about the url..."
+    single_url(song)
+    url = final_result['url']
+    title = final_result['title']
+    try:
+        downloader(url,title+".mp3")
+    except KeyboardInterrupt: #If we are interrupted by the user
+        print "\n[Soundcloud-dl]  Download cancelled by the user. "
+    sys.exit()
+
+
 # this tells us weather the script was directly run or not.
 # If it is directly run then the code bellow this comment executes.
 if __name__ == '__main__':
     print entrystring
 
     # we are asking the user that whether he wants to search for a song or a user
-    print "[Soundcloud-dl]  Do you want to search for a song or do you want to download the songs by a specific user? Type in 'song' or 'user' according to your requirement."
+    print "[Soundcloud-dl]  Do you want to search for a song or download the songs by a specific user or want to download a song by giving it's url ? Type in 'song' or 'user' or 'url' according to your requirement."
     what = raw_input("[Soundcloud-dl]  Your choice?\t")
 
     # here we are using a simple for loop to check whether the user typed song, user or anything else.
@@ -324,6 +400,8 @@ if __name__ == '__main__':
         for_song()
     elif what == "user":
         for_user()
+    elif what == "url":
+        for_url()
     else:
         print "[Soundcloud-dl]  your choice was wrong please run the program again\n"
         sys.exit()
